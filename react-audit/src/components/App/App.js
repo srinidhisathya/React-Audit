cc593d6e0fa162ecde2c9e8a6cd213635e1e7f9d
srisathy1 <srinidhi.sathyamurthy@publicissapient.com>
2021-04-07 12:14:15 +0530

    initial

---
+++ react-audit/src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Progress from '../Progress/Progress';
+import '../Progress/Progress.css';
+
+function App() {
+  return (
+    <Progress />
+  );
+}
+
+export default App;
---
+++ react-audit/src/components/Progress/Progress.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Progress(props) {
+    return (
+        <div>
+            <div className="parent">
+                <p className="progress">Progress 80%</p>
+            </div>
+        </div>
+    );
+}
+
+export default Progress;
